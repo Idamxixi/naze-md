88ab0a342b85c84030d43a97471c0e80e961d2dd
Idamxixi <95727431+Idamxixi@users.noreply.github.com>
2022-10-19 18:29:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6285691890852']
+global.ownernomer = "6285691890852"
+global.premium = ['6285691890852']
+global.packname = 'Sticker  Idam'
+global.author = 'Idamxixi'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
